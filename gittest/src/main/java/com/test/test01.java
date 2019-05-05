48af0970f16c554d989c11c020093cd5605c62ef
Administrator <Administrator@administrator>
2019-05-05 17:11:43 +0800

    test01 make 1

--- gittest/src/main/java/com/test/test01.java
+++ gittest/src/main/java/com/test/test01.java
@@ -3,5 +3,9 @@ package com.test;
 public class test01 {
 	public static void main(String[] args) {
 		System.out.println("hello @@@@");
+		System.out.println("hello @@@@");
+		System.out.println("hello @@@@");
+		System.out.println("hello @@@@");
+		System.out.println("hello @@@@");
 	}
 }

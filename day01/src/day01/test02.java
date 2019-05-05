72a1d76ea4663a2e108f8b0bbb41ba6a5d8dcef6
Administrator <Administrator@administrator>
2019-05-05 17:36:54 +0800

    123456

--- day01/src/day01/test02.java
+++ day01/src/day01/test02.java
@@ -3,5 +3,10 @@ package day01;
 public class test02 {
 	public static void main(String[] args) {
 		System.out.println("hello ,i am lieyuanren@@@@");
+		System.out.println("hello ,i am lieyuanren@@@@");
+		System.out.println("hello ,i am lieyuanren@@@@");
+		System.out.println("hello ,i am lieyuanren@@@@");
+		System.out.println("hello ,i am lieyuanren@@@@");
+		
 	}
 }	
